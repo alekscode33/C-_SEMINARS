d19b51253e248e3a8ae51c633efbd5a005143213
Alexander Sit <120254020+alekscode33@users.noreply.github.com>
2023-02-06 18:10:00 +1100

    Home Task 64

--- Sem09_HomeTasks/homeTask64/Program.cs
+++ Sem09_HomeTasks/homeTask64/Program.cs
@@ -5,5 +5,11 @@
      M = 4; N = 8. -> "4, 6, 7, 8"
 */
 
+void NumberFromMtoN(int m, int n)
+{
+    if (n > m) NumberFromMtoN(m, n - 1);
+    System.Console.Write($"{n} ");
+    if (m > n) NumberFromMtoN(m, n + 1);
 
-
+}
+NumberFromMtoN(4, 8);
